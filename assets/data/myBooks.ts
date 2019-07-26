ec8c9c647c8cc6080aea6b22aea3fc0726cedfd7
ravneet1996 <52752152+ravneet1996@users.noreply.github.com>
2019-07-26 13:53:41 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,14 @@
+import {Books} from '../../app/bookInfo';
+
+
+export const MYBOOKS: Books[] = 
+[
+    {bname:'Lonely Planet Portugal 10th Ed.', bauthor:'Lonely Lonely Planet, Regis St Louis, Kate Armstrong', 
+           bgenre:'Travel', byearPublished: 2017, bpicture:'assets/images/book1.jpg'},
+    {bname:'Project Smoke', bauthor:'Steven Raichlen', bgenre:'Cookbooks', byearPublished:2016, bpicture:'assets/images/book2.jpg'},
+    {bname:'The Things We Cannot Say', bauthor:'Kelly Rimmer', bgenre:'Fiction And Literature', 
+            byearPublished:2019, bpicture:'assets/images/book3.jpg'},
+    {bname:'Milk And Honey', bauthor:'Rupi Kaur', bgenre:'Poetry', byearPublished:2018, bpicture:'assets/images/book4.jpg'}
+
+
+]
